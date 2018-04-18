e33d8b3ac3c962e7bbc2dc17800eee06bc30118a
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-04-18 15:54:27 +0300

    Allow using UDPipe for automatic UCCA parsing by conversion from UD

--- semstr/scripts/parse_ud.py
+++ semstr/scripts/parse_ud.py
@@ -2,29 +2,72 @@
 
 import argparse
 import operator
+from itertools import tee
 
 from tqdm import tqdm
 from ucca import layer0
 from ucca.textutil import annotate_all, Attr
 
 from semstr.cfgutil import add_verbose_arg
-from semstr.conversion.conllu import ConlluConverter
+from semstr.conversion.conllu import ConlluConverter, from_conllu, to_conllu
 from semstr.convert import add_convert_args, CONVERTERS, write_passage, map_labels
 from semstr.evaluate import Scores, EVALUATORS
 from semstr.scripts.annotate import add_specs_args, read_specs
 
-desc = """Read passages in any format, extract text, parse using spaCy and save any format.
+desc = """Read passages in any format, extract text, parse using spaCy/UDPipe and save any format.
 NOTE: the dependencies output by spaCy depend on the model used.
 English models output CLEAR-style dependencies, not Universal Dependencies.
 See https://spacy.io/api/annotation#section-dependency-parsing"""
 
 
+def parse_spacy(passages, lang, verbose=False):
+    for passage, in annotate_all(zip(passages), as_array=True, as_tuples=True, lang=lang, verbose=verbose):
+        terminals = sorted(passage.layer(layer0.LAYER_ID).all, key=operator.attrgetter("position"))
+        dep_nodes = [ConlluConverter.Node()] + [ConlluConverter.Node(
+            t.position, terminal=t, token=ConlluConverter.Token(t.text, t.tag)) for t in terminals]
+        for dep_node in dep_nodes[1:]:
+            dep_node.token.paragraph = dep_node.terminal.paragraph
+            head = Attr.HEAD(dep_node.terminal.tok[Attr.HEAD.value])
+            if head:
+                head += dep_node.position
+            rel = Attr.DEP(dep_node.terminal.tok[Attr.DEP.value], lang=passage.attrib.get("lang", lang))
+            assert head is not None and rel is not None, \
+                "head=%r, rel=%r for token %d in %s" % (head, rel, dep_node.position, terminals)
+            edge = ConlluConverter.Edge(head, rel, remote=False)
+            dep_node.terminal = None
+            edge.link_head(dep_nodes)
+            dep_node.add_edges([edge])
+        parsed = ConlluConverter().build_passage(dep_nodes, passage.ID)
+        yield passage, parsed
+
+
+def parse_udpipe(passages, model_name, verbose=False):
+    from ufal.udpipe import Model, Pipeline, ProcessingError
+    model = Model.load(model_name)
+    if not model:
+        raise ValueError("Invalid model: '%s'" % model_name)
+    pipeline = Pipeline(model, "conllu", Pipeline.DEFAULT, Pipeline.DEFAULT, "conllu")
+    passages1, passages2 = tee(passages)
+    text = "\n".join(l for p in passages1 for l in to_conllu(p, tree=True))
+    error = ProcessingError()
+    processed = pipeline.process(text, error)
+    if verbose:
+        print(processed)
+    if error.occurred():
+        raise RuntimeError(error.message)
+    return zip(passages2, from_conllu(processed.splitlines(), passage_id=None))
+
+
+PARSERS = (SPACY, UDPIPE) = ("spacy", "udpipe")
+ANNOTATORS = {SPACY: parse_spacy, UDPIPE: parse_udpipe}
+
+
 def main(args):
-    for ps, out_dir, lang in read_specs(args):
+    for passages, out_dir, lang in read_specs(args):
         scores = []
-        for p, in annotate_all(zip(ps if args.verbose else tqdm(ps, unit=" passages", desc="Parsing " + out_dir)),
-                               as_array=True, as_tuples=True, lang=lang, verbose=args.verbose):
-            parsed = ConlluConverter().build_passage(get_dep_nodes(p, lang=p.attrib.get("lang", lang)), p.ID)
+        if not args.verbose:
+            passages = tqdm(passages, unit=" passages", desc="Parsing " + out_dir)
+        for passage, parsed in ANNOTATORS[args.parser](passages, lang, args.verbose):
             if args.write:
                 write_passage(parsed, args)
             else:
@@ -33,35 +76,16 @@ def main(args):
                 evaluator = EVALUATORS[args.output_format]
                 _, converter = CONVERTERS[args.output_format]
                 if converter is not None:
-                    parsed = converter(parsed)
-                    p = converter(p)
-                scores.append(evaluator.evaluate(parsed, p, verbose=args.verbose > 1))
+                    passage, parsed = map(converter, (passage, parsed))
+                scores.append(evaluator.evaluate(parsed, passage, verbose=args.verbose > 1))
         if scores:
             Scores(scores).print()
-
-
-def get_dep_nodes(passage, lang):
-    terminals = sorted(passage.layer(layer0.LAYER_ID).all, key=operator.attrgetter("position"))
-    dep_nodes = [ConlluConverter.Node()] + [ConlluConverter.Node(
-        t.position, terminal=t, token=ConlluConverter.Token(t.text, t.tag)) for t in terminals]
-    for dep_node in dep_nodes[1:]:
-        dep_node.token.paragraph = dep_node.terminal.paragraph
-        head = Attr.HEAD(dep_node.terminal.tok[Attr.HEAD.value], lang=lang)
-        if head:
-            head += dep_node.position
-        rel = Attr.DEP(dep_node.terminal.tok[Attr.DEP.value], lang=lang)
-        assert head is not None and rel is not None, \
-            "head=%r, rel=%r for token %d in %s" % (head, rel, dep_node.position, terminals)
-        edge = ConlluConverter.Edge(head, rel, remote=False)
-        dep_node.terminal = None
-        edge.link_head(dep_nodes)
-        dep_node.add_edges([edge])
-    return dep_nodes
 
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description=desc)
     add_specs_args(argparser)
+    argparser.add_argument("--parser", choices=PARSERS, default=SPACY, help="dependency parser to use (default: spacy)")
     argparser.add_argument("--output-format", choices=CONVERTERS, help="output file format (default: UCCA)")
     add_convert_args(argparser)
     argparser.add_argument("-e", "--evaluate", action="store_true", help="evaluate against original passages")
