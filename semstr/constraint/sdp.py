d812de29ad3d8a355639327c94a3244d85f0f653
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-04-29 16:07:09 +0300

    Allow node without head in SDP if it has a top child

--- semstr/constraint/sdp.py
+++ semstr/constraint/sdp.py
@@ -7,6 +7,6 @@ TOP_LEVEL = (SdpConverter.ROOT, SdpConverter.TOP)
 class SdpConstraints(Constraints):
     def __init__(self, args):
         super().__init__(args, top_level_allowed=TOP_LEVEL, top_level_only=TOP_LEVEL, allow_orphan_terminals=True,
-                         unique_outgoing={SdpConverter.HEAD}, required_outgoing={SdpConverter.HEAD},
+                         unique_outgoing={SdpConverter.HEAD}, required_outgoing={SdpConverter.HEAD, SdpConverter.TOP},
                          childless_incoming_trigger={SdpConverter.HEAD, "mwe"},
                          childless_outgoing_allowed={EdgeTags.Terminal, EdgeTags.Punctuation})
