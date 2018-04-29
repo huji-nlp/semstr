926f72eaa5c825cdc0c20203055b033e38c8e3e6
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-04-29 16:02:13 +0300

    Refactor validate.py and fix parent constraint

--- semstr/validate.py
+++ semstr/validate.py
@@ -50,51 +50,82 @@ def validate(passage, args):
     l0 = passage.layer(layer0.LAYER_ID)
     l1 = passage.layer(layer1.LAYER_ID)
     for terminal in l0.all:
+        yield from check_orphan_terminals(constraints, l1, terminal)
+        yield from check_multiple_incoming(constraints, terminal)
+    yield from check_top_level_allowed(constraints, l1)
+    for node in l1.all:
+        yield from check_multigraph(constraints, node)
+        yield from check_implicit_children(constraints, node)
+        yield from check_multiple_incoming(constraints, node)
+        yield from check_top_level_only(constraints, l1, node)
+        yield from check_required_outgoing(constraints, node)
+        yield from check_tag_rules(constraints, node)
+
+
+def check_orphan_terminals(constraints, l1, terminal):
+    if not constraints.allow_root_terminal_children:
         if not terminal.incoming:
             yield "Orphan %s terminal (%s) '%s'" % (terminal.tag, terminal.ID, terminal)
-        elif len(terminal.incoming) > 1:
-            yield "Reentrant %s terminal (%s) '%s'" % (terminal.tag, join(terminal.incoming), terminal)
-        if not constraints.allow_root_terminal_children and set(l1.heads).intersection(terminal.parents):
+        if set(l1.heads).intersection(terminal.parents):
             yield "Terminal child of root (%s) '%s'" % (terminal.ID, terminal)
+
+
+def check_top_level_allowed(constraints, l1):
     if constraints.top_level_allowed:
         for head in l1.heads:
             for edge in head:
                 if edge.tag not in constraints.top_level_allowed:
                     yield "Top level %s edge (%s)" % (edge.tag, edge)
-    for node in l1.all:
-        if not constraints.multigraph:
-            for parent_id, edges in groupby(node.incoming, key=lambda e: e.parent.ID):
-                edges = list(edges)
-                if len(edges) > 1:
-                    yield "Multiple edges from %s to %s (%s)" % (parent_id, node.ID, join(edges))
-        if constraints.require_implicit_childless and node.attrib.get("implicit") and len(node.outgoing) > 1:
-            yield "Implicit node with children (%s)" % node.ID
-        if constraints.possible_multiple_incoming:
-            incoming = [e for e in node.incoming if not e.attrib.get("remote") and
-                        e.tag not in constraints.possible_multiple_incoming]
-            if len(incoming) > 1:
-                yield "Multiple incoming non-remote (%s)" % join(incoming)
-        if constraints.top_level_only and node not in l1.heads:
-            for edge in node:
-                if edge.tag in constraints.top_level_only:
-                    yield "Non-top level %s edge (%s)" % (edge.tag, edge)
-        if constraints.required_outgoing and all(n.tag == layer1.NodeTags.Foundational for n in node.children) and \
-                not any(e.tag in constraints.required_outgoing for e in node):
-            yield "Non-terminal without outgoing %s (%s)" % (constraints.required_outgoing, node.ID)
-        for rule in constraints.tag_rules:
-            for edge in node:
-                for violation in (rule.violation(node, edge, Direction.outgoing, message=True),
-                                  rule.violation(edge.child, edge, Direction.incoming, message=True)):
-                    if violation:
-                        yield "%s (%s)" % (violation, join([edge]))
-                if not constraints.allow_edge(edge):
-                    "Illegal edge: %s" % join([edge])
-                if not constraints.allow_parent(edge.child, edge.tag):
-                    yield "%s may not be a '%s' parent (currently %s, %s)" % (
-                        node.ID, edge.tag, join(node.incoming), join(node))
-                if not constraints.allow_child(edge.child, edge.tag):
-                    yield "%s may not be a '%s' child (currently %s, %s)" % (
-                        edge.child.ID, edge.tag, join(edge.child.incoming), join(edge.child))
+
+
+def check_multigraph(constraints, node):
+    if not constraints.multigraph:
+        for parent_id, edges in groupby(node.incoming, key=lambda e: e.parent.ID):
+            edges = list(edges)
+            if len(edges) > 1:
+                yield "Multiple edges from %s to %s (%s)" % (parent_id, node.ID, join(edges))
+
+
+def check_implicit_children(constraints, node):
+    if constraints.require_implicit_childless and node.attrib.get("implicit") and len(node.outgoing) > 1:
+        yield "Implicit node with children (%s)" % node.ID
+
+
+def check_multiple_incoming(constraints, node):
+    if constraints.possible_multiple_incoming:
+        incoming = [e for e in node.incoming if not e.attrib.get("remote") and
+                    e.tag not in constraints.possible_multiple_incoming]
+        if len(incoming) > 1:
+            yield "Multiple incoming non-remote (%s)" % join(incoming)
+
+
+def check_top_level_only(constraints, l1, node):
+    if constraints.top_level_only and node not in l1.heads:
+        for edge in node:
+            if edge.tag in constraints.top_level_only:
+                yield "Non-top level %s edge (%s)" % (edge.tag, edge)
+
+
+def check_required_outgoing(constraints, node):
+    if constraints.required_outgoing and all(n.tag == layer1.NodeTags.Foundational for n in node.children) and \
+            not any(e.tag in constraints.required_outgoing for e in node):
+        yield "Non-terminal without outgoing %s (%s)" % (constraints.required_outgoing, node.ID)
+
+
+def check_tag_rules(constraints, node):
+    for rule in constraints.tag_rules:
+        for edge in node:
+            for violation in (rule.violation(node, edge, Direction.outgoing, message=True),
+                              rule.violation(edge.child, edge, Direction.incoming, message=True)):
+                if violation:
+                    yield "%s (%s)" % (violation, join([edge]))
+            if not constraints.allow_edge(edge):
+                "Illegal edge: %s" % join([edge])
+            if not constraints.allow_parent(node, edge.tag):
+                yield "%s may not be a '%s' parent (%s, %s)" % (node.ID, edge.tag, join(node.incoming), join(node))
+            if not constraints.allow_child(edge.child, edge.tag):
+                yield "%s may not be a '%s' child (%s, %s)" % (
+                    edge.child.ID, edge.tag, join(edge.child.incoming), join(edge.child))
 
 
 def main(args):
