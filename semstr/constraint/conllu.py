04eaea73e7025f676f2db5d4a3b373e68f799f0c
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-05-09 12:58:32 +0300

    Fix AttributeError

--- semstr/constraint/conllu.py
+++ semstr/constraint/conllu.py
@@ -1,11 +1,11 @@
 from ..constraints import Constraints, EdgeTags
-from ..conversion.conllu import ConlluConverter
+from ..conversion.conllu import ConlluConverter, PARATAXIS, PUNCT, AUX
 
 
 class ConlluConstraints(Constraints):
     def __init__(self, args):
         super().__init__(args, unique_outgoing={ConlluConverter.HEAD},
-                         required_outgoing={ConlluConverter.HEAD, ConlluConverter.PARATAXIS},
+                         required_outgoing={ConlluConverter.HEAD, PARATAXIS},
                          childless_incoming_trigger={ConlluConverter.HEAD},
-                         childless_outgoing_allowed={EdgeTags.Terminal, EdgeTags.Punctuation, ConlluConverter.PUNCT,
-                                                     ConlluConverter.HEAD, ConlluConverter.AUX})
+                         childless_outgoing_allowed={EdgeTags.Terminal, EdgeTags.Punctuation, PUNCT,
+                                                     ConlluConverter.HEAD, AUX})
