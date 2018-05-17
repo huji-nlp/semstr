743350f9f9e0a990eca9c5356a649a7f11883ef9
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-05-17 09:39:50 +0300

    Test for read_files_and_dirs with converters

---
+++ tests/test_io.py
@@ -0,0 +1,15 @@
+import os
+from glob import glob
+
+import pytest
+from ucca import layer0, layer1
+from ucca.ioutil import read_files_and_dirs
+
+from convert import FROM_FORMAT
+
+
+@pytest.mark.parametrize("suffix", ("xml", "amr", "sdp", "conllu"))
+def test_read_files_and_dirs(suffix):
+    for passage in read_files_and_dirs(glob(os.path.join("test_files", "*." + suffix)), converters=FROM_FORMAT):
+        assert passage.layer(layer0.LAYER_ID).all, "No terminals in passage " + passage.ID
+        assert len(passage.layer(layer1.LAYER_ID).all), "No non-terminals but the root in passage " + passage.ID
