504b9ed98fa2d4d24e1fefc691c51e4644d210a8
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-06-19 12:14:12 +0300

    Verify preservation of tokens when evaluating CoNLL-U

--- semstr/evaluation/conllu.py
+++ semstr/evaluation/conllu.py
@@ -1,3 +1,5 @@
+import os
+
 from ucca import evaluation
 from ucca.constructions import PRIMARY
 
@@ -8,7 +10,10 @@ EVAL_TYPES = (evaluation.LABELED, evaluation.UNLABELED)
 
 def get_scores(s1, s2, eval_type, verbose=False, units=False):
     converter = ConlluConverter()
-    edges = [[e for nodes, _ in converter.build_nodes(s) for n in nodes for e in n.outgoing] for s in (s1, s2)]
+    n1, n2 = list(map(list, list(map(converter.build_nodes, (s1, s2)))))
+    t1, t2 = list(map(join_tokens, (n1, n2)))
+    assert t1 == t2, "Tokens do not match: '%s' != '%s'" % diff(t1, t2)
+    edges = [[e for nodes, _ in dep_nodes for n in nodes for e in n.outgoing] for dep_nodes in (n1, n2)]
     for es in edges:
         for e in es:
             e.rel = None if eval_type == evaluation.UNLABELED else e.rel.partition(":")[0]
@@ -27,6 +32,16 @@ def get_scores(s1, s2, eval_type, verbose=False, units=False):
             print(r - g)
         res.print()
     return res
+
+
+def join_tokens(dep_nodes):
+    return "".join((n.parent_multi_word.token.text if n.position == n.parent_multi_word.position[0] else "")
+                   if n.parent_multi_word else n.token.text for nodes, _ in dep_nodes for n in nodes[1:])
+
+
+def diff(s1, s2):
+    start, end = [len(os.path.commonprefix((s1[::d], s2[::d]))) for d in (1, -1)]
+    return tuple(s[start - 1:-end] for s in (s1, s2))
 
 
 def evaluate(guessed, ref, converter=None, verbose=False, eval_types=EVAL_TYPES, units=False, **kwargs):
