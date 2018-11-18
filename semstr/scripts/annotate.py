55682b2ab776c28d430ec5c8a9e5d046f3044225
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-11-18 17:19:36 +0200

    Introduce placeholders in AMR conversion only after annotation
    Otherwise there might be a mismatch between the spaCy and UDPipe
    lemmatization, for example, resulting in invalid placeholders introduced
    into the UCCA passages.

--- semstr/scripts/annotate.py
+++ semstr/scripts/annotate.py
@@ -2,20 +2,23 @@
 
 import argparse
 from functools import partial
-
 from tqdm import tqdm
 from ucca import layer0
 from ucca.ioutil import write_passage, read_files_and_dirs, external_write_mode
 from ucca.textutil import annotate_all
 
 from semstr.cfgutil import read_specs, add_specs_args
-from semstr.convert import FROM_FORMAT, from_conllu
+from semstr.conversion.amr import AmrConverter
+from semstr.convert import FROM_FORMAT, from_conllu, from_amr
 from semstr.scripts.udpipe import annotate_udpipe, copy_tok_to_extra
 
 desc = """Read passages in any format, and write back with spaCy/UDPipe annotations."""
 
 
 CONVERTERS = {"conllu": partial(from_conllu, annotate=True)}
+
+FROM_FORMAT_NO_PLACEHOLDERS = dict(FROM_FORMAT)
+FROM_FORMAT_NO_PLACEHOLDERS.update({"amr": partial(from_amr, placeholders=False)})
 
 
 def copy_annotation(passages, conllu, as_array=True, verbose=False, lang=None):
@@ -33,7 +36,7 @@ def copy_annotation(passages, conllu, as_array=True, verbose=False, lang=None):
 
 
 def main(args):
-    for spec in read_specs(args, converters=FROM_FORMAT):
+    for spec in read_specs(args, converters=FROM_FORMAT_NO_PLACEHOLDERS):
         kwargs = dict(as_array=args.as_array, verbose=args.verbose, lang=spec.lang)
         passages = spec.passages
         if spec.udpipe:
@@ -43,6 +46,8 @@ def main(args):
         for passage in annotate_all(passages if args.verbose else
                                     tqdm(passages, unit=" passages", desc="Annotating " + spec.out_dir),
                                     replace=not spec.udpipe, **kwargs):
+            if passage.extra.get("format") == "amr" and args.as_array:
+                AmrConverter.introduce_placeholders(passage)
             write_passage(passage, outdir=spec.out_dir, verbose=args.verbose, binary=args.binary)
 
 
