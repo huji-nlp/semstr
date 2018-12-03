6aef3f3433ff817c26a5cc5a2fb99b668e98d736
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-12-03 14:34:00 +0200

    Add --no-enhanced option to visualize

--- semstr/scripts/visualize.py
+++ semstr/scripts/visualize.py
@@ -1,11 +1,12 @@
+import matplotlib.pyplot as plt
 import os
 from argparse import ArgumentParser
-
-import matplotlib.pyplot as plt
+from functools import partial
 from ucca import visualization
 from ucca.ioutil import get_passages_with_progress_bar, external_write_mode
 from ucca.normalization import normalize
 
+from semstr.cfgutil import add_boolean_option
 from semstr.convert import FROM_FORMAT, map_labels
 
 FROM_FORMAT["txt"] = FROM_FORMAT["amr"]
@@ -13,16 +14,17 @@ FROM_FORMAT["txt"] = FROM_FORMAT["amr"]
 if __name__ == "__main__":
     argparser = ArgumentParser(description="Visualize the given passages as graphs.")
     argparser.add_argument("passages", nargs="+", help="Passages in any format")
-    argparser.add_argument("--tikz", action="store_true", help="print tikz code rather than showing plots")
+    add_boolean_option(argparser, "tikz", "print tikz code rather than showing plots")
     argparser.add_argument("--out-dir", help="directory to save figures in (otherwise displayed immediately)")
-    group = argparser.add_mutually_exclusive_group()
-    group.add_argument("--no-normalize", action="store_false", dest="normalize", help="do not normalize passage")
-    group.add_argument("-e", "--extra-normalization", action="store_true", help="more normalization rules")
+    add_boolean_option(argparser, "normalize", "normalize passage", default=True)
+    add_boolean_option(argparser, "extra-normalization", "more normalization rules")
+    add_boolean_option(argparser, "enhanced", "read enhanced dependencies", default=True)
     argparser.add_argument("--label-map", help="CSV file specifying mapping of input edge labels to output edge labels")
-    argparser.add_argument("-i", "--node-ids", action="store_true", help="print tikz code rather than showing plots")
+    add_boolean_option(argparser, "node-ids", "print tikz code rather than showing plots", short="i")
     argparser.add_argument("-f", "--format", choices=("png", "svg"), default="png", help="image format")
     args = argparser.parse_args()
 
+    FROM_FORMAT = {k: partial(v, **vars(args)) for k, v in FROM_FORMAT.items()}
     if args.out_dir:
         os.makedirs(args.out_dir, exist_ok=True)
     for passage in get_passages_with_progress_bar(args.passages, desc="Visualizing", converters=FROM_FORMAT):
