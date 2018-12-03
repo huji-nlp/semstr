733b96e6d64f7d4f812fb9eaa5c3d9fb60d3d793
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-12-03 14:41:41 +0200

    Simplify promotion of advcl

--- semstr/conversion/conllu.py
+++ semstr/conversion/conllu.py
@@ -41,7 +41,7 @@ ROOT = "root"
 # trigger: immediate parent relation, recursive relations, forward?
 HIGH_ATTACHING = {CC: ((CONJ, ROOT), False, True),
                   MARK: ((ADVCL,), False, True),
-                  ADVCL: ((PARATAXIS, ROOT), (PARATAXIS, ROOT), False),
+                  ADVCL: ((ROOT,), False, False),
                   CONJ: ((PARATAXIS, ROOT), (PARATAXIS, ROOT), False),
                   PARATAXIS: ((ROOT,), False, False),
                   layer1.EdgeTags.Connector: ((CONJ,), False, True),
